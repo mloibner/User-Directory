b27aaecc0e90bdabfb9e4a313099d739d5ad9339
Monique Loibner <m.loibner@hotmail.com>
2020-10-13 18:34:25 +1100

    updated app.js file

--- src/App.js
+++ src/App.js
@@ -1,24 +1,26 @@
 //Ties whole app together, renders all components. state about results in here so that can sort and search as per requirements in readme.
 
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Header from "./components/Header";
 import Results from "./components/Results";
 import Search from "./components/Search";
-import TableRow from "./components/Table-Row";
 import Table from "./components/Table";
-import { BrowserRouter as Router, Route } from "react-router-dom";
+import { getUsers } from "./utils/api";
 
 function App() {
-    return (
-      <Router>
+  const [users, setUsers] = useState([]);
+  useEffect(() => {
+    getUsers().then((users) => {
+        setUsers(users.data.results);
+    });
+  }); 
+        return ( 
         <div>
           <Header />
           <Search />
-          <Table />
-          <TableRow />
+          <Table users = { users }/> 
           <Results />          
         </div>
-      </Router>
     );
   }
   
--- src/components/Table.js
+++ src/components/Table.js
@@ -1,1 +1,9 @@
 //write JSX to render the html style and event listeners
+
+function Table(props) {
+    return (<div>
+        { props.users.map((user) => {
+            <div>{ user.name }</div>
+        }) }
+    </div>)
+}
